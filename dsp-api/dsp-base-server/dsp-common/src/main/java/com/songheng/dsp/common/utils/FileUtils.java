5c2ac356e2c811549c7689bf93e579deb8d8a85e
zhangshuai <zhangshuai@021.com>
2019-01-25 11:13:26 +0800

    文件工具类

--- dsp-api/dsp-base-server/dsp-common/src/main/java/com/songheng/dsp/common/utils/FileUtils.java
+++ dsp-api/dsp-base-server/dsp-common/src/main/java/com/songheng/dsp/common/utils/FileUtils.java
@@ -1,8 +1,8 @@
 package com.songheng.dsp.common.utils;
 
+import com.google.common.base.Charsets;
 import com.google.common.io.Files;
 import lombok.extern.slf4j.Slf4j;
-
 import java.io.File;
 
 /**
@@ -16,6 +16,29 @@ import java.io.File;
  **/
 @Slf4j
 public class FileUtils {
+
+    /**
+     * @Description: 写文件
+     * @param writeFilePath 写入的文件路径
+     * @param content   写入的文件内容
+     * @param append   是否追加
+     * @return 写入成功返回true 写入失败返回false
+     **/
+    public static boolean writeFile(String writeFilePath,String content,boolean append){
+        try {
+            if(append){
+                Files.append(content, new File(writeFilePath), Charsets.UTF_8);
+            }else {
+                Files.write(content, new File(writeFilePath), Charsets.UTF_8);
+            }
+            log.debug("[write file]:path={}&append={}&content:{}",writeFilePath,append,content);
+            return true;
+        }catch (Exception e){
+            log.error("path={}&append={}&content={}\t{}",writeFilePath,append,content,e);
+            return false;
+        }
+    }
+
     /**
      * @Description: 复制文件
      * @param originFilePath 原始文件路径
@@ -25,19 +48,33 @@ public class FileUtils {
     public static boolean copyFile(String originFilePath,String copyFilePath){
         try {
             Files.copy(new File(originFilePath), new File(copyFilePath));
+            log.debug("[copy file]:origin={}&copy={}",originFilePath,copyFilePath);
             return true;
         }catch (Exception e){
-            log.error("{}&{}\t{}",originFilePath,copyFilePath,e);
+            log.error("origin={}&copy={}\t{}",originFilePath,copyFilePath,e);
             return false;
         }
     }
-
-    public static void main(String[] args) {
-        long base = System.currentTimeMillis();
-        for (int i = 0; i < 100000; i++) {
-            log.error("{}&{}\t{}","aaaaaadfdfadsfadfadfadfadaaaaaa","bbbbbbadfdsafadfadsfadfasfabbbb","ccccccsdfadsfadsfadadfadfadfadadfadsfadfadfa");
+    /**
+     * @Description: 移动、重命名文件
+     * @param originFilePath 原始文件路径
+     * @param moveFilePath   需要复制的文件路径
+     * @return 复制成功返回true 复制失败返回false
+     **/
+    public static boolean moveFile(String originFilePath,String moveFilePath){
+        try {
+            Files.move(new File(originFilePath), new File(moveFilePath));
+            log.debug("[move file]:origin={}&move:{}",originFilePath,moveFilePath);
+            return true;
+        }catch (Exception e){
+            log.error("origin={}move=&{}\t{}",originFilePath,moveFilePath,e);
+            return false;
         }
-        long end = System.currentTimeMillis();
-        System.out.println(base+"\t"+end+"\t"+(end-base));
+    }
+    public static void main(String[] args) {
+        writeFile("/data/1.txt","hello,world8",true);
+        copyFile("/data/1.txt","/data/2.txt");
+        moveFile("/data/1.txt","/data/2.txt");
+
     }
 }
