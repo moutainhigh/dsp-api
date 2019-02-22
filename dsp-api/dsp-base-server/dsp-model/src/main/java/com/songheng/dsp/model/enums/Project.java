fa0b0a92e7690888c17d2dbce5137b6c59861ebe
zhangshuai <zhangshuai@021.com>
2019-02-22 17:18:37 +0800

    log4j2

--- dsp-api/dsp-base-server/dsp-model/src/main/java/com/songheng/dsp/model/enums/Project.java
+++ dsp-api/dsp-base-server/dsp-model/src/main/java/com/songheng/dsp/model/enums/Project.java
@@ -26,9 +26,9 @@ public enum Project {
      * */
     private String projectName;
     /**
-     * 项目别名
+     * 项目终端
      * */
-    private String alis;
+    private String terminal;
 
     /**
      * 设备类型
@@ -40,12 +40,28 @@ public enum Project {
      * */
     private String describe;
 
-    Project(String projectName,String alis,String deviceType,String describe){
+    Project(String projectName,String terminal,String deviceType,String describe){
         this.projectName = projectName;
-        this.alis = alis;
+        this.terminal = terminal;
         this.deviceType = deviceType;
         this.describe = describe;
     }
-
-
+    /**
+     * 是否h5项目
+     * */
+    public static boolean isH5(String terminal){
+        return H5.getTerminal().equalsIgnoreCase(terminal);
+    }
+    /**
+     * 是否h5项目
+     * */
+    public static boolean isApp(String terminal){
+        return APP.getTerminal().equalsIgnoreCase(terminal);
+    }
+    /**
+     * 是否h5项目
+     * */
+    public static boolean isPc(String terminal){
+        return PC.getTerminal().equalsIgnoreCase(terminal);
+    }
 }
