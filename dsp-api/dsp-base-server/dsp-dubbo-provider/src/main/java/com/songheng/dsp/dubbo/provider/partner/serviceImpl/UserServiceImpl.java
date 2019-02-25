4723bb5ddd63d565b9949013d9f38848895d1d0f
luoshaobing <luoshaobing@021.com>
2019-02-25 11:22:54 +0800

    pom.xml优化

--- dsp-api/dsp-base-server/dsp-dubbo-provider/src/main/java/com/songheng/dsp/dubbo/provider/partner/serviceImpl/UserServiceImpl.java
+++ dsp-api/dsp-base-server/dsp-dubbo-provider/src/main/java/com/songheng/dsp/dubbo/provider/partner/serviceImpl/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package com.songheng.dsp.dubbo.provider.partner.serviceImpl;
+package com.songheng.dsp.dubbo.provider.partner.serviceimpl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.songheng.dsp.dubbo.baseinterface.partner.service.UserService;
